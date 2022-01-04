607ebb67cc6904cda79abdeb04f5d2ff810fc057
Axlerod21 <94286705+Axlerod21@users.noreply.github.com>
2022-01-04 15:43:01 +0200

    Update hw.js

--- hw.js
+++ hw.js
@@ -1,24 +1,42 @@
-let urls = {
-    link1:"https://www.facebook.com/",
-    link2:"https://www.google.com/",
-    link3:"https://www.amazon.com/", 
-    link4:"https://www.youtube.com/", 
-    link5:"https://www.instagram.com/", 
-    link6:"https://www.whatsapp.com/"
-};
-
-links = document.querySelectorAll(".link");
-
-links.forEach((x) => {
-    x.setAttribute("href", urls[x.getAttribute("blabla")]);
-});
-
-const shuffle = (links, urls) => {
+const shuffle = (l, dict) => {  
+        
     let i;
-    for(i = 0; i < links.length; i++) {
-        links[i].setAttribute("href", urls["link" + (Math.floor(Math.random() * 6) + 1)]);
-        console.log(links[i]);
+    for (i = 0; i < l.length; i++) 
+    {
+        l[i].setAttribute("href", dict["link" +( 
+        Math.floor(Math.random() * 6) + 1)]);
+
+        l[i].setAttribute("class", "a" + ( 
+        Math.floor(Math.random() * 7) + 1));
+        console.log(l[i]);
     }
 };
+let urls = {
+    "link1": "https://he-il.facebook.com/",
+    "link2": "https://www.google.com/", 
+    "link3": "https://www.amazon.com/",
+    "link4": "https://he-il.facebook.com/",
+    "link5": "https://www.google.com/", 
+    "link6": "https://www.amazon.com/"
+};
+links = document.querySelectorAll(".pageHeaderMenuLink > a");
+            
+setInterval(()=>shuffle(links, urls), 5000);
 
-setInterval(() => shuffle(links, urls), 5000);
+var flag = 0;
+const changeHide = (elem) => {
+    if(flag = 0) {
+        removeHide(elem);
+        flag = 1;
+    }
+    else {
+        addHide(elem);
+        flag = 0;
+    }
+}
+const removeHide = (elem) => {
+    elem.classList.remove("hide");
+}
+const addHide = (elem) => {
+    elem.classList.add("hide");
+}
